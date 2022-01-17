fdec446dd10b4863bbe615ae6f4caa1a6ca6fd97
JesDanFors <90110706+JesDanFors@users.noreply.github.com>
2022-01-17 14:39:00 +0100

    Initial commit

---
+++ CustomerManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using TurboCollections;
+
+namespace CustomerManagement{
+    class program{
+        static void Main(string[] args){
+            var turboList = new TurboList<float>();
+        }
+    }
+}
---
+++ TurboCollections/TurboList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TurboCollections{
+    public class TurboList<T>{
+
+        public int Count => 5;
+        public TurboList(){
+            Console.WriteLine("Hello turbo!");
+        }
+
+        public void Add(int i){
+            
+        }
+    }
+}
+
