a6e096a2169c221e7ceebd00d5943b5c4ed65454
JesDanFors <90110706+JesDanFors@users.noreply.github.com>
2022-01-17 16:29:44 +0100

    After class update

--- TurboCollections.Test/TurboListTests.cs
+++ TurboCollections.Test/TurboListTests.cs
@@ -15,5 +15,30 @@ namespace TurboCollections.Test{
             list.Add(5);
             Assert.AreEqual(1,list.Count);
         }
+
+        [Test, TestCase(5), TestCase(7)]
+        public void AddingMultipleElementsIncreasesTheCount(int numberOfElements){
+            var list = new TurboList<int>();
+            for (int i = 0; i < numberOfElements; i++)
+                list.Add(5);
+            Assert.AreEqual(numberOfElements, list.Count);
+            
+        }
+
+        [Test]
+        public void AnAddedElementCanBeGet(){
+            var list = new TurboList<int>();
+            list.Add(1337);
+            Assert.AreEqual(1337, list.Get(0));
+        }
+
+        [Test]
+        public void ClearTheEntireList(){
+            var list = new TurboList<int>();
+            for (int i = 0; i < 10; i++){
+                list.Add(1);
+            }
+            Assert.Zero(list.Count);
+        }
     }
 }
--- TurboCollections/TurboList.cs
+++ TurboCollections/TurboList.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 
+
 namespace TurboCollections{
     public class TurboList<T>{
+        
+        int size;
+        T[] items = Array.Empty<T>();
 
-        public int Count => 5;
-        public TurboList(){
-            Console.WriteLine("Hello turbo!");
+        public int Count => items.Length;
+        
+        // adds one item to the end of the list.
+        public void Add(T item){
+            T[] newArray = new T[Count + 1];
+            for (int i = 0; i < Count; i++){
+                newArray[i] = items[i];
+            }
+            newArray[Count] = item;
+            items = newArray;
         }
-
-        public void Add(int i){
-            
+        
+        // gets the item at the specified index. If the index is outside the correct range, an exception is thrown.
+        public T Get(int index){
+            if (index < items.Length){
+                return items[index];
+            }
+            else{
+                throw new Exception("This index is out of bounds!");
+            }
         }
     }
 }
