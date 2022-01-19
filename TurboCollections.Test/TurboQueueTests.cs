d11e40ac3b20d4be6ca02ca28cf1b48c0f1a8d6a
JesDanFors <90110706+JesDanFors@users.noreply.github.com>
2022-01-19 16:17:41 +0100

    Added test and function for Enqueue and peak

--- TurboCollections.Test/TurboQueueTests.cs
+++ TurboCollections.Test/TurboQueueTests.cs
@@ -1,10 +1,28 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
+using TurboCollections;
 
 namespace TurboQueueTests{
     public class TurboQueueTests{
         [Test]
         public void EmptyQueue(){
-            
+            var newQueue = new TurboQueue<int>();
+            Assert.AreEqual(0, newQueue.Count);
+        }
+
+        [Test]
+        public void AddToTheQueue(){
+            var newQueue = new TurboQueue<int>();
+            newQueue.Enqueue(5);
+            Assert.AreEqual(1,newQueue.Count);
+        }
+
+        [Test]
+        public void CorrectItemAddedToQueue(){
+            var newQueue = new TurboQueue<int>();
+            newQueue.Enqueue(5);
+            newQueue.Enqueue(4);
+            Assert.AreEqual(5, newQueue.Peak());
         }
     }
 }    
---
+++ TurboCollections/TurboQueue.cs
@@ -0,0 +1,22 @@
+﻿namespace TurboCollections;
+
+public class TurboQueue<T>{
+    T[] Queue = Array.Empty<T>();
+    
+    public int Count{ get; private set; }
+
+    public void Enqueue(T item){
+        T[] newArray = new T[Count + 1];
+        newArray[0] = item;
+        for (int i = Count-1; i >= 1; i--){
+            newArray[i] = Queue[i];
+            Console.WriteLine(newArray[i]);
+        }
+        Queue = newArray;
+        Count++;
+    }
+
+    public T Peak(){
+        return Queue[^1];
+    }
+}
