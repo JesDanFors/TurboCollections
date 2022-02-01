a722ec698ac0593a994b2b302da98cabc61e78f5
JesDanFors <90110706+JesDanFors@users.noreply.github.com>
2022-02-01 15:35:06 +0100

    made base for turboTree

---
+++ TurboCollections/TurboTree.cs
@@ -0,0 +1,24 @@
+﻿namespace TurboCollections;
+
+public class TurboTree{
+    int[] tree;
+    int[] leftTree;
+    int[] rightTree;
+    int _base;
+
+    void Insert(int insertedNumber,int[] tree){
+        if (_base == null){
+            this.tree[0] = insertedNumber;
+            return;
+        }
+
+        if (_base!= null){
+            if (insertedNumber>_base){
+                Insert(insertedNumber,rightTree);
+            }
+            else{
+                Insert(insertedNumber,leftTree);
+            }
+        }
+    }
+}
