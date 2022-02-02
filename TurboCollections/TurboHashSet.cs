b15a58e6fe9d68ce2067df60062054bab8efa323
JesDanFors <90110706+JesDanFors@users.noreply.github.com>
2022-02-02 10:46:34 +0100

    Start implementing the TurboHashSet

---
+++ TurboCollections/TurboHashSet.cs
@@ -0,0 +1,44 @@
+﻿namespace TurboCollections;
+
+public class TurboHashSet<T>{
+    T[] hashSet = Array.Empty<T>();
+    int items = 0;
+    public bool Insert(T item){
+        items++;
+        if (hashSet.Length < MathF.Round(1.3f * items)){
+            Resize();
+        }
+        return true;
+    }
+
+    public bool Exists(T item){
+        
+    }
+
+    public bool Remove(T item){
+        
+    }
+
+    void Resize(){
+        var tempArray = new TurboHashSet<T>[GetNextPrime(hashSet.Length)];
+        for (int i = 0; i < hashSet.Length; i++){
+            tempArray[i].Insert(hashSet[i]);
+        }
+    }
+
+    int GetNextPrime(int prevPrime){
+        while(true){
+            bool isPrime = true;
+            prevPrime = prevPrime + 1;
+            int squaredNumber = (int)Math.Sqrt(prevPrime);
+            for (int i = 2; i <= squaredNumber; i++){
+                if (prevPrime % i == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime)
+                return prevPrime;
+        }
+    }
+}
